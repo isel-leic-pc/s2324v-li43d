51a4ff031a502f20363d387ab23125b2f800694d
jmartins3 <jmartinsisel.pt>
2024-03-10 10:40:50 +0000

    lab 1 tests

--- lab1/src/main/kotlin/Main.kt
+++ lab1/src/main/kotlin/Main.kt
@@ -1,5 +1,30 @@
 package pt.isel.pc
 
+import kotlin.concurrent.thread
+
+fun launchSomeThreads(concurrencyLevel: Int, action : (Int)->Unit ) : List<Thread> {
+    return (1 .. concurrencyLevel).map {id->
+        thread {
+            action(id)
+        }
+    }
+}
+
 fun main() {
-    println("Hello World!")
+    val redThreads = launchSomeThreads(10)  { id->
+        repeat(10) { action->
+            println("simulating some word done by red thread $id, action $action")
+        }
+    }
+    val greenThreads = launchSomeThreads(10)  { id->
+        repeat(20) { action ->
+            println("simulating some word done by green thread $id, action $action")
+        }
+    }
+    greenThreads.forEach {
+        it.join()
+    }
+    redThreads.forEach {
+        it.join()
+    }
 }
